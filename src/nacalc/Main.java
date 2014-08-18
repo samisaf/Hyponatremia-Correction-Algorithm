5c0cb6d570c0c3cb67b8f207f8f3593df9016634
Sami Safadi <samisaf@gmail.com>
2014-08-17 22:16:18 -0500

    First commit
    Application is still non-function. Basic interface built.

---
+++ src/nacalc/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nacalc;
+
+/**
+ *
+ * @author Sami
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // (NaEnd*TBW - NaEnd*Time*URate - NaStart*TBW + Time*UK*URate + Time*UNa*URate)/(Time*(IVFConc - NaEnd))
+        FluidsFrame.main(args);
+
+    }
+    
+}
